04c03ce6ca883308b6a443bea8a0805fa9c884f4
subham-bharati <subhambharati10@gmail.com>
2026-05-22 12:38:23 +0530

    Fix

--- src/main/java/com/ecommerce/project/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/project/config/SecurityConfig.java
@@ -55,7 +55,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:5173",
+                "https://*.vercel.app",
+                "https://*.up.railway.app"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept"));
         configuration.setExposedHeaders(List.of("Authorization"));
